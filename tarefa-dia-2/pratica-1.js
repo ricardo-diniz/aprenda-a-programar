acd56331b708e683b87257d61b77060fe6034349
Ricardo <ricardodiniz42@gmail.com>
2022-08-28 21:54:39 -0300

    paragrafo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,7 +1,7 @@
 let nome = "Fulano de Tal"
 let email = "contato@email.com"
 let profissao = "profissao"
-let idade = "18"
+let idade = 18
 let lugar = "Cuba"
 
 console.log(nome)
@@ -10,4 +10,6 @@ console.log(profissao)
 console.log(idade)
 console.log(lugar)
 
-console.log ('olá, ${nome} ! Somos')
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
+
+
