e62beb8b693d2e84a1b02142ff08c50ec7ef59e2
Ricardo <ricardodiniz42@gmail.com>
2022-08-28 21:38:11 -0300

    pratica 1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,13 @@
+let nome = "Fulano de Tal"
+let email = "contato@email.com"
+let profissao = "profissao"
+let idade = "18"
+let lugar = "Cuba"
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
+
+console.log ('olá, ${nome} ! Somos')
