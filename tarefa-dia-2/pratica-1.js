aff0970c4c7d1b8f9e0cdc91a3bb4ce8bc160242
Ricardo <ricardodiniz42@gmail.com>
2022-08-28 22:23:17 -0300

    pratica 2

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -11,5 +11,3 @@ console.log(idade)
 console.log(lugar)
 
 console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
-
-
