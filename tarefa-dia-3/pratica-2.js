2cfcb83dbec5ba4e7250a1c377669ec5b7fd37b1
Ricardo <ricardodiniz42@gmail.com>
2022-08-30 04:02:27 -0300

    show odd numbers until given number

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,11 @@
+// tabuada
+let n = 9
+let repeticoes = 10
+let i = 1
+
+while (i<=repeticoes) {
+    let result = n * i
+    let texto = `${n} x ${i} = ${result}`
+    console.log(texto)
+    i++
+}
